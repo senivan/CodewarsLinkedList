6a467118cfe20d635b3c657136aa406dcbba0034
Ivan Sen <senivan1997@gmail.com>
2024-04-03 20:20:52 +0300

    Fourth task from simple done

---
+++ Simple-4/solution.py
@@ -0,0 +1,18 @@
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+    
+def get_nth(node, index):
+    if node is None:
+        raise ValueError()
+    if index < 0:
+        raise ValueError()
+    for i in range(index):
+        node = node.next
+        if node is None:
+            raise ValueError()
+    return node
